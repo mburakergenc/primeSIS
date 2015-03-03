5958b36851d187ce8c514aff40eff040b7bf2893
Muhammet Burak Ergenc <muhammetergenc@gmail.com>
2015-03-02 20:53:32 -0600

    Update 2014_10_12_000000_create_users_table.php
    Deleted Unnecessary fields.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,6 @@ class CreateUsersTable extends Migration {
 		Schema::create('users', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('avatar');
-			$table->string('username');
 			$table->string('name');
 			$table->string('email')->unique();
 			$table->string('password', 60);
